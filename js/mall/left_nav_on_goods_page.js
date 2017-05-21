c15c82f54be8bff104e9ccd9b84203580b78be12
xyf <425746853@qq.com>
2017-05-21 13:31:51 +0800

    分页按钮点击js完成

--- js/mall/left_nav_on_goods_page.js
+++ js/mall/left_nav_on_goods_page.js
@@ -49,6 +49,82 @@ $(function () {
     $('header').click(function () {
         hideParticular();
     });
+
+    //new
+    bindClick();
+    //  直接点击页数
+    function bindClick() {
+        $('.pagination_div ul li').click(function () {
+            var cur = $(".pagination_div ul li.current_page").children("a").html();
+            $(".pagination_div ul li.current_page").removeClass("current_page");
+            $(this).addClass("current_page");
+            //  点击的页数
+            var which_click = $(this).children("a").html();
+            //  在if里面处理
+            if (cur!=which_click) {
+
+            }
+        });
+    }
+
+    //  上一页
+    $('.pagination_lt').click(function () {
+        var current = $('.pagination_div ul li.current_page');
+        var temp = current.children("a").html();
+        //  已经达到最左边，再点无反应
+        if (temp==1) {
+            return false;
+        }
+        updateCurrent(current,1,temp);
+        //      这个就是当前的页数
+        var current_page = $('.pagination_div ul li.current_page').children("a").html();
+
+    });
+    //下一页
+    $('.pagination_gt').click(function () {
+        var current = $('.pagination_div ul li.current_page');
+        var temp = current.children("a").html();
+        // 到达最右边
+        if (temp==99) {
+            return false;
+        }
+        updateCurrent(current,2,temp);
+        var current_page = $('.pagination_div ul li.current_page').children("a").html();
+        //      通过这个current_page 来获取数据
+
+    });
+
+    // temp 当前的值（1,2,3,4...）
+    function updateCurrent(current,to,temp) {
+    //    1左，2右
+        var num = current.nextAll().length;
+        if (to==1) {
+            if (num==4) {
+                current.siblings(":last").remove();
+                current.before("<li><a>"+(temp-1)+"</a></li>");
+            }
+            if (num==3) {
+                if (!(temp-2<1)) {
+                    current.siblings(":last").remove();
+                    current.siblings(":first").before("<li><a>"+(temp-2)+"</a></li>");
+                }
+            }
+            current.removeClass("current_page");
+            current.prev().addClass("current_page");
+        }else {
+            if (num==0) {
+                current.siblings(":first").remove();
+                current.after("<li><a>"+(parseInt(temp)+1)+"</a></li>");
+            }
+            if (num==1) {
+                current.siblings(":first").remove();
+                current.siblings(":last").after("<li><a>"+(parseInt(temp)+2)+"</a></li>");
+            }
+            current.removeClass("current_page");
+            current.next().addClass("current_page");
+        }
+        bindClick();
+    }
 });
 function hideParticular() {
     if ($('.particular_type_div').is(":visible")) {
